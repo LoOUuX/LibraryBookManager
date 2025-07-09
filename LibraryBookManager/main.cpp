5d2b5ee46022f870a4f7d34e53706230556df69c
E <e>
2025-07-09 18:07:33 +1000

    Initial Commit

---
+++ LibraryBookManager/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LibraryBookManager
+//
+//  Created by Ehsan Sadeghian on 9/7/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
