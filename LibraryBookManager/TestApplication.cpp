3b51f767b48ff20bb7fc12ba90bd4746c262f5d1
E <e>
2025-08-06 17:36:29 +1000

    Test application

--- LibraryBookManager/TestApplication.cpp
+++ LibraryBookManager/TestApplication.cpp
@@ -2,63 +2,11 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include "Book.h"
 using namespace std;
 
-class Book {
-protected:
-    string title;
-    string author;
-    int isbn;
-    bool available;
-    string dateAdded;
 
-public:
-    Book() {}
 
-    Book(string t, string a, int i, bool avail, string date) {
-        title = t;
-        author = a;
-        isbn = i;
-        available = avail;
-        dateAdded = date;
-    }
-
-    void setBookDetails(string t, string a, int i, bool avail, string date) {
-        title = t;
-        author = a;
-        isbn = i;
-        available = avail;
-        dateAdded = date;
-    }
-
-    void displayBookDetails() const {
-        cout << "Title: " << title << "\nAuthor: " << author
-             << "\nISBN: " << isbn << "\nAvailable: " << (available ? "Yes" : "No")
-             << "\nDate Added: " << dateAdded << endl;
-    }
-
-    int getISBN() const {
-        return isbn;
-    }
-
-    bool isAvailable() const {
-        return available;
-    }
-
-    void borrowBook() {
-        if (available) {
-            available = false;
-            cout << "Book borrowed successfully.\n";
-        } else {
-            cout << "Book is not available.\n";
-        }
-    }
-
-    void returnBook() {
-        available = true;
-        cout << "Book returned successfully.\n";
-    }
-};
 
 // Sorting function by ISBN
 void sortBookData(vector<Book>& books) {
